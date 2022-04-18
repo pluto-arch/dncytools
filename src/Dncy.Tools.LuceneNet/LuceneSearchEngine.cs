2d37cb47d90e82e29571d81a31924558aecdf181
张玉龙 <620524a2924.cdb@sina.cn>
2022-04-18 17:34:11 +0800

    优化包

--- src/Dncy.Tools.LuceneNet/LuceneSearchEngine.cs
+++ src/Dncy.Tools.LuceneNet/LuceneSearchEngine.cs
@@ -6,7 +6,6 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 using System.Text;
-using System.Threading.Tasks;
 using Dncy.Tools.LuceneNet.Models;
 
 using Lucene.Net.Analysis;
