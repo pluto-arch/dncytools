a3ad172d3d2b32171835a288544598d32852d461
张玉龙 <620524a2924.cdb@sina.cn>
2022-07-01 18:34:05 +0800

    依赖注入生成器

---
+++ tests/DIGeneratorTest/DemoA.cs
@@ -0,0 +1,6 @@
+﻿namespace DIGeneratorTest;
+
+public class DemoA
+{
+    
+}
---
+++ tests/DIGeneratorTest/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using DIGeneratorTest;
+using SA = DIGeneratorTest.ServiceA.DemoService;
+using SB = DIGeneratorTest.ServiceB.DemoService;
+using Microsoft.Extensions.DependencyInjection;
+
+Console.WriteLine("Hello, World!");
+
+
+
+
+var services = new ServiceCollection();
+
+services.AutoInjectDIGeneratorTest();
+var provider = services.BuildServiceProvider();
+
+
+var serA = provider.GetRequiredService<SA>();
+serA.P();
+
+var serb = provider.GetRequiredService<SB>();
+serb.P();
+
+
+
+Console.ReadKey();
+
+
+
+
+
