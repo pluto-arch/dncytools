e895721d092274d57d42209142f5c6beac582b68
张玉龙 <620524a2924.cdb@sina.cn>
2022-07-26 18:49:09 +0800

    excel ZipArchive

---
+++ src/Dncy.Tools.Excel/Zip/ZipPackageInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.IO.Compression;
+
+namespace Dncy.Tools.Excel.Zip
+{
+    public class ZipPackageInfo
+    {
+        public ZipArchiveEntry ZipArchiveEntry { get; set; }
+        public string ContentType { get; set; }
+        public ZipPackageInfo(ZipArchiveEntry zipArchiveEntry, string contentType)
+        {
+            this.ZipArchiveEntry = zipArchiveEntry;
+            ContentType = contentType;
+        }
+    }
+}
+
