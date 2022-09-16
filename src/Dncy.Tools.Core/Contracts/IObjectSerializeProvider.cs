ce15958995978a57b1529ff1f6637ecfc46f7cca
张玉龙 <620524a2924.cdb@sina.cn>
2022-09-16 16:51:10 +0800

    添加序列化器协定

---
+++ src/Dncy.Tools.Core/Contracts/IObjectSerializeProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Dncy.Tools.Core
+{
+    public interface IObjectSerializeProvider
+    {
+        string Serialize(object obj);
+
+        T Deserialize<T>(string objStr);
+
+        object Deserialize(string objStr, Type type);
+    }
+}
