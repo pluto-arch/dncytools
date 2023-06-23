a76c27a17f4f560ae0bfc6f6f2c3ec79861f7ba7
张玉龙 <620524a2924.cdb@sina.cn>
2023-06-23 11:42:33 +0800

    添加返回类型封装，升级7.0

---
+++ src/Dncy.Tools.Core/Models/Return.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Dncy.Tools.Core
+{
+    /// <summary>
+    /// 方法返回值包装器
+    /// </summary>
+    /// <remarks>避免方法执行尽用抛异常方式进行错误返回</remarks>
+    /// <typeparam name="T">返回数据类型</typeparam>
+    /// <typeparam name="E">错误信息类型</typeparam>
+    public record Return<T, E>
+    {
+        private readonly bool _success;
+        private readonly T value;
+        private readonly E error;
+
+        private Return(T v, E e, bool success)
+        {
+            value = v;
+            error = e;
+            _success = success;
+        }
+
+
+        public bool Successed => _success;
+
+
+        public T Data => value;
+
+        public E Errors => error;
+
+
+        public static Return<T, E> Success(T v)
+        {
+            return new(v, default(E), true);
+        }
+
+        public static Return<T, E> Error(E e)
+        {
+            return new(default(T), e, false);
+        }
+
+
+        public static implicit operator Return<T, E>(T v) => new(v, default(E), true);
+        public static implicit operator Return<T, E>(E e) => new(default(T), e, false);
+
+        public R Match<R>(Func<T, R> success, Func<E, R> failure)
+            => _success ? success(value) : failure(error);
+    }
+
+
+
+    /// <summary>
+    /// 方法返回值包装器
+    /// </summary>
+    /// <remarks>避免方法执行尽用抛异常方式进行错误返回</remarks>
+    /// <typeparam name="E">错误信息类型</typeparam>
+    public record ReturnVoid
+    {
+        private readonly bool _success;
+        private readonly string error;
+        private ReturnVoid(string e, bool success)
+        {
+            error = e;
+            _success = success;
+        }
+        public bool Successed => _success;
+        public string Errors => error;
+        public static ReturnVoid Error(string e)
+        {
+            return new(e, false);
+        }
+        public static ReturnVoid Successe()
+        {
+            return new(default, true);
+        }
+
+        public static implicit operator ReturnVoid(string e) => new(e, false);
+    }
+}
