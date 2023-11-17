98e7b2d174efe129015c9d2ff2800dcd9d078154
yulong <620524a2924.cdb@sina.cn>
2023-11-17 18:29:35 +0800

    升级net8

--- tests/DIGeneratorTest/DemoA.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-
-namespace DIGeneratorTest;
-
-[Injectable(InjectLifeTime.Singleton)]
-public class DemoA
-{
-    
-}
--- tests/SearchEngineAspnetMvcTest/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SearchEngineAspnetMvcTest.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
